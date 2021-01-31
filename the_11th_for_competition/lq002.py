f2a6fe5c0d42ebd09003d84165cb546fc9f32b4b
52hertzi <x552240_a@163.com>
2021-01-31 21:55:39 +0800

    2021.1.31

---
+++ the_11th_for_competition/lq002.py
@@ -0,0 +1,6 @@
+#节点数
+point_num = 2019
+#线条数
+line_num = point_num * (point_num - 1)/2
+print(line_num)
+
---
+++ the_11th_for_competition/lq003.py
@@ -0,0 +1,7 @@
+list = ['L','A','N','Q','I','A','O']
+#元素个数
+num = len(list)
+
+for i in range(1,num):
+    num *= i
+print(num)
