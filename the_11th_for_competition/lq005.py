6f1a82b3ef1f544fb68ca516960c46ef42de4b53
52hertzi <x552240_a@163.com>
2021-02-04 22:50:15 +0800

    modify wrong

--- the_11th_for_competition/lq005.py
+++ the_11th_for_competition/lq005.py
@@ -4,6 +4,8 @@ try:
     list(content)
     for i in content:
         str = ord(i) + 3        #转换为Ascii码
+        if(str > ord('z')):
+            str = str - ord('z') + ord('a') - 1
         print(chr(str),end='')
 except Exception as e:
     print("输入有误")
